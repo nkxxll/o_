52881475980ed9284264be9901efbbd5fd4688a3
nkirschall <niki.nice1203@gmail.com>
2023-08-10 13:24:38 +0200

    feat: first web server in Ocaml

--- bin/args.ml
+++ bin/args.ml
@@ -1,8 +1,8 @@
-let port = ref "4321"
+let port = ref 4321
 let usage = "o_htmx_caml -p <port>\ndefault is 4321"
 
 let speclist =
-  [ ("-p", Arg.Set_string port, "Port for the htmx webserver to run on") ]
+  [ ("-p", Arg.Set_int port, "Port for the htmx webserver to run on") ]
 
 let anon_fun anonymous_arg = print_endline ("arg: " ^ anonymous_arg ^ " is not allowed see usage with -h/--help")
 
--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,9 @@
-let () = 
+let hello name =
+  "<html>\n\t<body>\n\t<h1>Hello " ^ name ^ "</h1>\n\r\t</body>\n\r</html>"
+
+let () =
   Args.get_args;
-  print_endline ("This is the port " ^ !Args.port)
+  print_endline ("Web Server will be started on: " ^ (Int.to_string !Args.port));
+  Dream.run ~interface:"127.0.0.1" ~port:!Args.port
+  @@ Dream.logger
+  @@ Dream.router [ Dream.get "/" (fun _ -> Dream.html (hello "buddy")) ]
