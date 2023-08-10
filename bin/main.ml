d553445041e5207611efcdae6400cab2cb2b65fc
nkirschall <niki.nice1203@gmail.com>
2023-08-11 00:44:13 +0200

    feat: first post with lwt async * is a promise here with body the /echo returns the request data

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,15 @@
-let hello name =
-  "<html>\n\t<body>\n\t<h1>Hello " ^ name ^ "</h1>\n\r\t</body>\n\r</html>"
+let hello name = "<html>\n\t<body>\n\t<h1>Hello " ^ name ^ "</h1>\n\r\t</body>\n\r</html>"
 
 let () =
   Args.get_args;
-  print_endline ("Web Server will be started on: " ^ (Int.to_string !Args.port));
+  print_endline ("Web Server will be started on: " ^ Int.to_string !Args.port);
   Dream.run ~interface:"127.0.0.1" ~port:!Args.port
   @@ Dream.logger
-  @@ Dream.router [ Dream.get "/" (fun _ -> Dream.html (hello "buddy")) ]
+  @@ Dream.router
+       [ Dream.get "/" (fun _ -> Dream.html (hello "buddy"))
+       ; Dream.post "/echo" (fun request ->
+           let open Lwt.Syntax in
+           let* body = Dream.body request in
+           Dream.respond ~headers:[ "Content-Type", "application/octet-stream" ] body)
+       ]
+;;
